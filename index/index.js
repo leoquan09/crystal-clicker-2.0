fe546683c70f5284461303d1f6a4bb18f155cbdc
Leo <238378041+leoquan09@users.noreply.github.com>
2026-08-19 22:29:19 +0000

    hopefully squashed bug this time...

--- index/index.js
+++ index/index.js
@@ -2,7 +2,8 @@ const crystal = document.getElementById('crystal-img');
 const scoreText = document.getElementById('crystal-count');
 const title = document.querySelector('.game-title');
 
-let score = parseInt(localStorage.getItem('scores')) || 0; 
+let savedScore = parseInt(localStorage.getItem('scores')); 
+let score = isNaN(savedScore) ? 0 : savedScore;
 scoreText.textContent = score;
 
 crystal.addEventListener('click', () => { 
