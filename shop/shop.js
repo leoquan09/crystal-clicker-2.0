46d494ca38886f49ca54be3344e6e02988b36b97
Leo <leoquan09@gmail.com>
2026-09-13 15:39:50 -0700

    Added shop logic to increase levels and power completed upgrades 1 and 2 but need to add the actual functionality to the game to complete the adding crystals in the index game.

---
+++ shop/shop.js
@@ -0,0 +1,62 @@
+let score = isNaN(parseFloat(localStorage.getItem('scores'))) ? 0 : parseFloat(localStorage.getItem('scores'));
+let scoreText = document.getElementById('crystal-count');
+
+scoreText.textContent = score;
+
+const upgrade1 = {
+    btn: document.getElementById('buy-1'),
+    costText: document.getElementById('cost1'),
+    lvl: isNaN(parseInt(localStorage.getItem('upgradeOneLvl'))) ? 0 : parseInt(localStorage.getItem('upgradeOneLvl')),
+    power: isNaN(parseFloat(localStorage.getItem('upgradeOnePower'))) ? 1 : parseFloat(localStorage.getItem('upgradeOnePower')),
+    cost: isNaN(parseFloat(localStorage.getItem('upgradeOneCost'))) ? 20 : parseFloat(localStorage.getItem('upgradeOneCost'))
+};
+
+upgrade1.costText.textContent = `Level ${upgrade1.lvl + 1} cost: ${upgrade1.cost} crystals`;
+
+const upgrade2 = {
+    btn: document.getElementById('buy-2'),
+    costText: document.getElementById('cost2'),
+    lvl: isNaN(parseInt(localStorage.getItem('upgradeTwoLvl'))) ? 0 : parseInt(localStorage.getItem('upgradeTwoLvl')),
+    power: isNaN(parseFloat(localStorage.getItem('upgradeTwoPower'))) ? 0 : parseFloat(localStorage.getItem('upgradeTwoPower')),
+    cost: isNaN(parseFloat(localStorage.getItem('upgradeTwoCost'))) ? 100 : parseFloat(localStorage.getItem('upgradeTwoCost'))
+};
+
+upgrade2.costText.textContent = `Level ${upgrade2.lvl + 1} cost: ${upgrade2.cost} crystals`;
+
+upgrade1.btn.addEventListener('click', () => {
+    if (score >= upgrade1.cost) {
+        score -= upgrade1.cost; 
+        upgrade1.lvl += 1; 
+        upgrade1.power = upgrade1.power * 1.1; 
+        upgrade1.cost = Math.round(upgrade1.cost * 1.15); 
+  
+        scoreText.textContent = score;
+        upgrade1.costText.textContent = `Level ${upgrade1.lvl + 1} cost: ${upgrade1.cost} crystals`; 
+
+        localStorage.setItem('scores', score);
+        localStorage.setItem('upgradeOneLvl', upgrade1.lvl);
+        localStorage.setItem('upgradeOnePower', upgrade1.power);
+        localStorage.setItem('upgradeOneCost', upgrade1.cost);
+    }
+});
+
+
+upgrade2.btn.addEventListener('click', () => {
+    if (upgrade2.lvl === 0) {
+        upgrade2.lvl += 5;
+        if (score >= upgrade1.cost) {
+            score -= upgrade1.cost; 
+            upgrade1.lvl += 1; 
+            upgrade1.power = upgrade1.power * 1.1; 
+            upgrade1.cost = Math.round(upgrade1.cost * 1.15); 
+    
+            scoreText.textContent = score;
+            upgrade2.costText.textContent = `Level ${upgrade2.lvl + 1} cost: ${upgrade2.cost} crystals`; 
+
+            localStorage.setItem('scores', score);
+            localStorage.setItem('upgradeTwoLvl', upgrade2.lvl);
+            localStorage.setItem('upgradeTwoPower', upgrade2.power);
+            localStorage.setItem('upgradeTwoCost', upgrade2.cost);
+        }
+    }
+});
